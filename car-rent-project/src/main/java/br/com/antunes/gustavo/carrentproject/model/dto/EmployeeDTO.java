4c9f1d6c2391aaf8b30e09c774c8d5a99b7aa554
Gustavo <gustavo.antunes.rocha@outlook.com>
2023-04-04 09:29:48 -0300

    Add. Getters and Setters for EmployeeDTO

--- car-rent-project/src/main/java/br/com/antunes/gustavo/carrentproject/model/dto/EmployeeDTO.java
+++ car-rent-project/src/main/java/br/com/antunes/gustavo/carrentproject/model/dto/EmployeeDTO.java
@@ -33,5 +33,77 @@ public class EmployeeDTO {
         this.salary = employee.getSalary();
     }
 
-    // getters and setters omitted for brevity
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getIdentificationNumber() {
+		return identificationNumber;
+	}
+
+	public void setIdentificationNumber(String identificationNumber) {
+		this.identificationNumber = identificationNumber;
+	}
+
+	public AddressDTO getAddress() {
+		return address;
+	}
+
+	public void setAddress(AddressDTO address) {
+		this.address = address;
+	}
+
+	public String getJobTitle() {
+		return jobTitle;
+	}
+
+	public void setJobTitle(String jobTitle) {
+		this.jobTitle = jobTitle;
+	}
+
+	public BigDecimal getSalary() {
+		return salary;
+	}
+
+	public void setSalary(BigDecimal salary) {
+		this.salary = salary;
+	}
+
+    
 }
