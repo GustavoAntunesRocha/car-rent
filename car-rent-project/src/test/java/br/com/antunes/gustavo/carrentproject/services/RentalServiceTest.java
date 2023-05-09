5166b3a56b5b6ae4b483d6a8ba4d363b871ae855
Gustavo <gustavo.antunes.rocha@outlook.com>
2023-05-09 11:12:55 -0300

    Fixed rental service test

--- car-rent-project/src/test/java/br/com/antunes/gustavo/carrentproject/services/RentalServiceTest.java
+++ car-rent-project/src/test/java/br/com/antunes/gustavo/carrentproject/services/RentalServiceTest.java
@@ -9,13 +9,14 @@ import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.assertj.core.api.Assertions;
-import org.assertj.core.api.RecursiveComparisonAssert;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
 
 import br.com.antunes.gustavo.carrentproject.model.Address;
 import br.com.antunes.gustavo.carrentproject.model.City;
@@ -26,19 +27,26 @@ import br.com.antunes.gustavo.carrentproject.model.RentalStatus;
 import br.com.antunes.gustavo.carrentproject.model.State;
 import br.com.antunes.gustavo.carrentproject.model.Vehicle;
 import br.com.antunes.gustavo.carrentproject.model.VehicleType;
+import br.com.antunes.gustavo.carrentproject.model.repository.CityRepository;
 import br.com.antunes.gustavo.carrentproject.model.repository.CustomerRepository;
 import br.com.antunes.gustavo.carrentproject.model.repository.RentalRepository;
 import br.com.antunes.gustavo.carrentproject.model.repository.VehicleRepository;
+import br.com.antunes.gustavo.carrentproject.service.CustomerService;
 import br.com.antunes.gustavo.carrentproject.service.RentalService;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 
 @SpringBootTest
+@ContextConfiguration
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@WithUserDetails("admin@admin.com")
 public class RentalServiceTest {
 	
 	@Autowired
 	private RentalService rentalService;
+
+	@Autowired
+	private CustomerService customerService;
 	
 	@Autowired
 	private RentalRepository rentalRepository;
@@ -49,6 +57,9 @@ public class RentalServiceTest {
 	@Autowired
 	private VehicleRepository vehicleRepository;
 
+	@Autowired
+	private CityRepository cityRepository;
+
 
 	private Rental rental;
 
@@ -58,6 +69,7 @@ public class RentalServiceTest {
 
 	
 	
+	@Order(0)
 	@BeforeEach
 	@Transactional
 	public void createEntity() {
@@ -76,9 +88,11 @@ public class RentalServiceTest {
 	    customer.setEmail("teste@teste.com");
 	    customer.setIdentificationNumber("123");
 	    customer.setDriverLicenceNumber("123456789");
+
+		City city = cityRepository
+                .save(new City(1L, "Goiania", new State(1L, "Goias", "GO", new Country(1L, "Brasil", "55"))));
 	    customer.setAddress(
-	    		new Address(1L, "Rua 5", "740000", new City(1L, "Goiania", new State(1L, "Goias", "GO", new Country(1L, "Brasil", "55"))))
-	    		);
+	    		new Address(1L, "Rua 5", "740000", city ));
 	    
 	    LocalDate startDate = LocalDate.of(2023, 4, 12);
 	    LocalDate endDate = LocalDate.of(2023, 4, 30);
@@ -90,7 +104,7 @@ public class RentalServiceTest {
 	    rentals.add(rental);
 	    customer.setRentals(rentals);
 
-		customerRepository.save(rental.getCustomer());
+		customerService.create(customerService.convertToDTO(customer), "123");
 		vehicleRepository.save(rental.getVehicle());
 	    
 	}
@@ -99,6 +113,7 @@ public class RentalServiceTest {
 	@Transactional
 	public void save() {
 		
+
 		
 		rentalService.create(rentalService.convertToDTO(rental));
 
