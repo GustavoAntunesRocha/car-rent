306fb7afd92b88dacac1444634a56b2ce7f3bfed
Gustavo <gustavo.antunes.rocha@outlook.com>
2023-05-09 09:09:15 -0300

    Fixed Employee controller test

--- car-rent-project/src/test/java/br/com/antunes/gustavo/carrentproject/controllers/EmployeeControllerTest.java
+++ car-rent-project/src/test/java/br/com/antunes/gustavo/carrentproject/controllers/EmployeeControllerTest.java
@@ -5,25 +5,18 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 import java.math.BigDecimal;
-import java.util.Map;
 
-import org.json.JSONObject;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
@@ -32,7 +25,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import br.com.antunes.gustavo.carrentproject.controller.EmployeeController;
 import br.com.antunes.gustavo.carrentproject.controller.LoginRequest;
 import br.com.antunes.gustavo.carrentproject.controller.LoginResponse;
-import br.com.antunes.gustavo.carrentproject.controller.UserController;
 import br.com.antunes.gustavo.carrentproject.exception.RestExceptionHandler;
 import br.com.antunes.gustavo.carrentproject.model.Address;
 import br.com.antunes.gustavo.carrentproject.model.City;
@@ -41,7 +33,6 @@ import br.com.antunes.gustavo.carrentproject.model.State;
 import br.com.antunes.gustavo.carrentproject.model.dto.AddressDTO;
 import br.com.antunes.gustavo.carrentproject.model.dto.EmployeeDTO;
 import br.com.antunes.gustavo.carrentproject.model.repository.CityRepository;
-import br.com.antunes.gustavo.carrentproject.service.EmployeeService;
 import br.com.antunes.gustavo.carrentproject.service.UserService;
 
 @SpringBootTest
@@ -51,9 +42,6 @@ public class EmployeeControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
-
-    @MockBean
-    private EmployeeService employeeService;
 
     @Autowired
     private CityRepository cityRepository;
@@ -68,23 +56,8 @@ public class EmployeeControllerTest {
 
     private String jwt;
 
-    /* @Order(2)
-    @BeforeAll
-    public void setUp() throws Exception {
-
-        String email = "admin@admin.com";
-        String password = "admin";
-        Map<String, Object> response = Map.of("email", email, "password", password);
-        ObjectMapper mapper = new ObjectMapper();
-        
-        this.mockMvc.perform(post("/api/v1/user/login")
-        .content(mapper.writeValueAsString(response))
-        .contentType("application/json"))
-                .andExpect(status().isOk()).andDo(result -> {
-                    JSONObject responseBody = new JSONObject(result.getResponse().getContentAsString());
-                    this.jwt = responseBody.getString("accesToken");
-                });
-    } */
+    record createEmployeeRequest(EmployeeDTO employeeDTO, String password) {
+    }
 
     @Order(2)
     @BeforeAll
@@ -110,7 +83,8 @@ public class EmployeeControllerTest {
         employeeDTO.setIdentificationNumber("123");
         employeeDTO.setJobTitle("Sales manager");
         employeeDTO.setSalary(BigDecimal.valueOf(2500));
-        City city = cityRepository.save(new City(1L, "Goiania", new State(1L, "Goias", "GO", new Country(1L, "Brasil", "55"))));
+        City city = cityRepository
+                .save(new City(1L, "Goiania", new State(1L, "Goias", "GO", new Country(1L, "Brasil", "55"))));
         employeeDTO.setAddress(
                 new AddressDTO(new Address(1L, "Rua 5", "740000",
                         city)));
@@ -119,60 +93,60 @@ public class EmployeeControllerTest {
 
     @Test
     public void testCreateEmployee() throws Exception {
-        String expectedJson = new ObjectMapper().writeValueAsString(this.employeeDTO);
+
+        String expectedJson = new ObjectMapper().writeValueAsString(new createEmployeeRequest(employeeDTO, "admin"));
         this.mockMvc.perform(post("/api/v1/employee")
-        .content(expectedJson)
-        .header("Authorization", "Bearer " + this.jwt)
-        .contentType("application/json"))
+                .content(expectedJson)
+                .header("Authorization", "Bearer " + this.jwt)
+                .contentType("application/json"))
                 .andExpect(status().isCreated());
     }
 
     @Test
     public void testCreateEmployeeWithoutAuthorization() throws Exception {
-        String expectedJson = new ObjectMapper().writeValueAsString(this.employeeDTO);
+        String expectedJson = new ObjectMapper().writeValueAsString(new createEmployeeRequest(employeeDTO, "admin"));
         this.mockMvc.perform(post("/api/v1/employee")
-        .content(expectedJson)
-        .contentType("application/json"))
+                .content(expectedJson)
+                .contentType("application/json"))
                 .andExpect(status().isForbidden());
     }
 
     @Test
-    public void testUpdateEmployeeShouldBeTrue() throws Exception{
-        String originalJson = new ObjectMapper().writeValueAsString(this.employeeDTO);
+    public void testUpdateEmployeeShouldBeTrue() throws Exception {
+        String originalJson = new ObjectMapper().writeValueAsString(new createEmployeeRequest(employeeDTO, "admin"));
         this.mockMvc.perform(post("/api/v1/employee")
-        .content(originalJson)
-        .header("Authorization", "Bearer " + this.jwt)
-        .contentType("application/json"));
-        
+                .content(originalJson)
+                .header("Authorization", "Bearer " + this.jwt)
+                .contentType("application/json"));
+
         this.employeeDTO.setFirstName("Jose");
-        String updatedJson = new ObjectMapper().writeValueAsString(this.employeeDTO);
+        String updatedJson = new ObjectMapper().writeValueAsString(new createEmployeeRequest(employeeDTO, "admin"));
         this.mockMvc.perform(put("/api/v1/employee")
-        .content(updatedJson)
-        .header("Authorization", "Bearer " + this.jwt)
-        .contentType("application/json"))
+                .content(updatedJson)
+                .header("Authorization", "Bearer " + this.jwt)
+                .contentType("application/json"))
                 .andExpect(status().isOk());
     }
 
     @Test
-    public void testDeleteEmployeeShouldBeTrue() throws Exception{
-        String originalJson = new ObjectMapper().writeValueAsString(this.employeeDTO);
+    public void testDeleteEmployeeShouldBeTrue() throws Exception {
+        String originalJson = new ObjectMapper().writeValueAsString(new createEmployeeRequest(employeeDTO, "admin"));
         this.mockMvc.perform(post("/api/v1/employee")
-        .content(originalJson)
-        .header("Authorization", "Bearer " + this.jwt)
-        .contentType("application/json"))
+                .content(originalJson)
+                .header("Authorization", "Bearer " + this.jwt)
+                .contentType("application/json"))
                 .andExpect(status().isCreated());
 
         this.mockMvc.perform(delete("/api/v1/employee")
-        .header("Authorization", "Bearer " + this.jwt)
-        .param("id", 1L + "")
-        .contentType("application/json"))
+                .header("Authorization", "Bearer " + this.jwt)
+                .param("id", 1L + "")
+                .contentType("application/json"))
                 .andExpect(status().isOk());
 
         this.mockMvc.perform(get("/api/v1/employee")
-        .header("Authorization", "Bearer " + this.jwt)
-        .param("id", 6L + "")
-        .contentType("application/json"))
-                .andDo(print())
+                .header("Authorization", "Bearer " + this.jwt)
+                .param("id", 6L + "")
+                .contentType("application/json"))
                 .andExpect(status().isNotFound());
     }
 
